68fb113cf295447d758d731bc7e9906229fb4eb7
Ceglarz <oski.ceglarz@gmail.com>
2018-06-04 17:16:57 +0200

    Creating TrainingsAPI and DbContext

---
+++ Workout-server/TrainingsAPI/Models/TrainingDbContext.cs
@@ -0,0 +1,19 @@
+﻿using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace TrainingsAPI.Models
+{
+    public class TrainingDbContext : DbContext
+    {
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+            optionsBuilder.UseSqlServer
+           ("Server=(localdb)\\mssqllocaldb;Database=TrainingDb;Trusted_Connection=True;");
+        }
+
+        public DbSet<Training> Trainings { get; set; }
+    }
+}
